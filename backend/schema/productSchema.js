0affc36d6175e6011bff283119c07e2700a2af08
mani254 <manifreelancer25@gmail.com>
2024-09-17 22:09:46 +0530

    added the schema for the admin, collection, product, review, user and admin

---
+++ backend/schema/productSchema.js
@@ -0,0 +1,135 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+
+function formatFloat(value) {
+   return parseFloat(value).toFixed(2);
+}
+
+const productSchema = new mongoose.Schema({
+   title: {
+      type: String,
+      required: [true, 'Title is required'],
+      unique: [true, 'Title is already existed'],
+      maxlength: [100, 'Title should be less than 100 characters'],
+      trim: true,
+   },
+   slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+   },
+   shortDescription: {
+      type: String,
+      required: [true, 'Short description is required'],
+      maxlength: [400, 'Short description should be less than 400 characters'],
+      trim: true
+   },
+   longDescription: {
+      type: String,
+   },
+   variants: [
+      {
+         name: {
+            type: String,
+            required: [true, 'Variant name is required'],
+         },
+         images: {
+            type: [String],
+            required: [true, 'Images are required'],
+            validate: {
+               validator: function (arr) {
+                  return arr.length > 0;
+               },
+               message: 'There must be at least one image for each variant',
+            },
+         },
+      },
+   ],
+   price: {
+      type: Number,
+      required: [true, 'Price is required'],
+      set: formatFloat,
+   },
+   comparePrice: {
+      type: Number,
+      set: formatFloat,
+   },
+   GST: {
+      type: Number,
+      set: formatFloat,
+   },
+   stock: {
+      type: Number,
+      required: [true, 'Stock is required'],
+      min: [0, 'Stock cannot be negative'],
+      default: 0,
+   },
+   SKU: {
+      type: Number,
+      required: [true, 'SKU is required'],
+      unique: [true, 'SKU is already existed'],
+   },
+   metaTitle: {
+      type: String,
+      maxlength: [100, 'Meta title should be less than 100 characters'],
+   },
+   metaDescription: {
+      type: String,
+      maxlength: [400, 'Meta description should be less than 400 characters'],
+   },
+   keywords: {
+      type: [String],
+   },
+   collection: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'Collection',
+      },
+   ],
+   tags: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'Tag',
+      },
+   ],
+   reviews: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'Review'
+      }
+   ],
+   status: {
+      type: String,
+      enum: ['active', 'inactive', 'archived'],
+      default: 'active',
+   },
+   // createdBy: {
+   //    type: mongoose.Schema.Types.ObjectId,
+   //    ref: 'Admin',
+   //    required: True
+   // },
+   // updatedBy: {
+   //    type: mongoose.Schema.Types.ObjectId,
+   //    ref: 'Admin'
+   // },
+   deletedAt: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Admin'
+   }
+}, {
+   timestamps: true
+})
+
+
+productSchema.pre('save', function (next) {
+   if (!this.slug && this.title) {
+      this.slug = slugify(this.title, { lower: true, strict: true });
+   }
+   next();
+})
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
