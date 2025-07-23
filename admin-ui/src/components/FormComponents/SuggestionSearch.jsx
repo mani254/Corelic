48e9d9b49f1e302c5e715365295cd34ec180b4b4
mani254 <manikantadev254@gmail.com>
2025-07-23 18:41:27 +0530

    fresh-start for the  turbo repo setup along with best structured data and best design and code

--- admin-ui/src/components/FormComponents/SuggestionSearch.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import SingleSuggestionSearch from "./SingleSuggestionSearch";
-import MultiSuggestionSearch from "./MultiSuggestionSearch";
-
-const SuggestionSearch = ({ single = false, ...props }) => {
-	return single ? <SingleSuggestionSearch {...props} /> : <MultiSuggestionSearch {...props} />;
-};
-
-export default SuggestionSearch;
--- admin-ui/src/redux/actions/modalActions.js
+++
@@ -1,8 +0,0 @@
-export const showModal = (props, component) => ({
-   type: "SHOW_MODAL",
-   payload: { props, component },
-});
-
-export const hideModal = () => ({
-   type: "HIDE_MODAL",
-});
